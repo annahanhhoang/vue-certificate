1ac990bc086c82d30f41e8e10a097baee30fffd4
Anna Hoang <anna.hoang1322@gmail.com>
2023-12-25 21:14:37 -0600

    First commit

---
+++ new-years-countdown/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import CountdownHeader from "@/components/CountdownHeader.vue";
+import CountdownSegment from "@/components/CountdownSegment.vue";
+import { ref, onMounted } from "vue";
+
+const days = ref(0);
+const hours = ref(0);
+const minutes = ref(0);
+const seconds = ref(0);
+
+onMounted(() => {
+  const targetDate = new Date(`${new Date().getFullYear() + 1}-01-01T00:00:00`).getTime();
+  calculateCountdown(targetDate);
+  const interval = setInterval(() => {
+    calculateCountdown(targetDate);
+  }, 1000);
+});
+
+const calculateCountdown = (targetDate) => {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+  days.value = Math.floor(distance / (1000 * 60 * 60 * 24));
+  hours.value = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  minutes.value = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  seconds.value = Math.floor((distance % (1000 * 60)) / 1000);
+}
+</script>
+<template>
+  <div class="app-wrapper">
+    <div class="countdown-box">
+      <CountdownHeader />
+      <main class="flex justify-center">
+        <CountdownSegment data-test="days" label="days" :number="days" />
+        <CountdownSegment data-test="hours" label="hours" :number="hours" />
+        <CountdownSegment data-test="minutes" label="minutes" :number="minutes" />
+        <CountdownSegment data-test="seconds" label="seconds" :number="seconds" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app-wrapper {
+  @apply flex items-center justify-center w-full h-full p-10;
+}
+
+.countdown-box {
+  @apply shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px];
+}
+
+body {
+  @apply bg-gray-100;
+}
+</style>
